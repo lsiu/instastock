fe3d4d1981ed081c3caf3e95984ae5b4e737ebdd
Edward Kwok <edward_kwok@ymail.com>
2015-03-22 04:32:02 +0800

    Fixed Content Type and the Content-Encoding of the send money post

--- src/main/java/com/instastore/service/CheckoutService.java
+++ src/main/java/com/instastore/service/CheckoutService.java
@@ -5,44 +5,55 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Objects;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.catalina.connector.Response;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class CheckoutService {
 
-	private AtomicLong transactionNumber = new AtomicLong(4000000001010101020L);
-
 	public CheckoutResult checkout(CheckoutProduct product) {
-		HttpClient client = HttpClientBuilder.create().build();
+		HttpClient client = HttpClientBuilder.create()
+				.disableCookieManagement()
+				.disableContentCompression()
+				.disableAuthCaching()
+				.disableConnectionState().build();
 		byte[] encoded;
 		try {
+			Random random=new Random();
+			long transactionNumber=random.nextInt(1000000000)+4000000000000000000L;
 			HttpPost httpPost = new HttpPost(
 					"http://dmartin.org:8021/moneysend/v2/transfer");
-			httpPost.addHeader("Content-Type", "application/xml");
 			
 			encoded = Files.readAllBytes(Paths
 					.get("src/main/resources/xml.xml"));
 			String xml = new String(encoded, StandardCharsets.UTF_8);
 			String replaced=xml.replace("{transaction_reference}",
-					String.valueOf(transactionNumber.addAndGet(1)))
+					String.valueOf(transactionNumber))
 					.replace("{funding_account_number}",
 					product.getBuyerCreditCard())
 					.replace("{receiving_card}", product.getSellerCreditCard())
 					.replace("{price}", String.valueOf((long)Math.floor((product.getPrice()))));
-			HttpEntity entity = new ByteArrayEntity(replaced.getBytes(StandardCharsets.UTF_8));
+			StringEntity entity = new StringEntity(replaced);
+			entity.setContentType((new BasicHeader(HTTP.CONTENT_TYPE, "application/xml")));
+			entity.setContentEncoding("UTF-8");
 			httpPost.setEntity(entity);
+
+			
 			HttpResponse response = client.execute(httpPost);
 			if (Objects.equals(Response.SC_OK, response.getStatusLine()
 					.getStatusCode())) {
@@ -52,6 +63,8 @@ public class CheckoutService {
 			} else {
 				log.info("Response of send money is {}",
 						response.getStatusLine());
+				ResponseHandler<String> handler = new BasicResponseHandler();
+				log.info(handler.handleResponse(response));
 				CheckoutResult checkoutResult = new CheckoutResult();
 				checkoutResult.setSuccess(false);
 				return checkoutResult;
